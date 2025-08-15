8a3b2117d68ef4f7fbee30dc2c0aeb799e2d204a
Wellington Arantes <w.aranteslfc@gmail.com>
2025-08-15 20:32:00 -0300

    feat: add biome as project linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 
 export function App() {
-  return (
-    <div className="flex h-screen justify-center items-center">
-      <Button>Lead Forge</Button>
-    </div>
-  )
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<Button>Lead Forge</Button>
+		</div>
+	);
 }
